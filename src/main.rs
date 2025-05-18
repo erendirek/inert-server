93c602fd5252d6793888015cc343f2f693562745
Eren Direk <erendirek2145@gmail.com>
2025-05-18 15:44:19 +0300

    cors layer added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use axum::{Extension, Router};
 
+use hyper::Method;
 use inert_server::{database::create_db_pool, routes::rest::setup_rest_index_router};
 use inert_server::utils::env_loader::{load_env_variables, EnvVars};
+use tower_http::cors::{Any, CorsLayer};
+
+fn setup_cors_layer() -> CorsLayer {
+    CorsLayer::new().allow_origin(Any).allow_methods([Method::GET, Method::POST, Method::DELETE, Method::PATCH, Method::OPTIONS])
+}
 
 #[tokio::main]
 async fn main() -> Result<(), &'static str>{
@@ -14,7 +20,8 @@ async fn main() -> Result<(), &'static str>{
     let app = Router::new()
         .nest("/api/rest", setup_rest_index_router())
         .layer(Extension(dbp_shared_state.clone()))
-        .layer(Extension(vars.clone()));
+        .layer(Extension(vars.clone()))
+        .layer(setup_cors_layer());
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     println!("listening on {}", addr);
